f56d8af15db56d9fb51635d6ee6bdb8de52a2de3
Rahul <rahulavunuri6@gmail.com>
2025-10-27 10:33:39 +0530

    added jenkins

--- src/main/java/com/cognizant/selenium/App.java
+++ src/main/java/com/cognizant/selenium/App.java
@@ -1,10 +1,31 @@
-package com.cognizant.selenium;
-
-/**
- * Hello world!
- */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+//package com.cognizant.selenium;
+//
+//
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.io.IOException;
+// 
+//import org.apache.poi.ss.usermodel.Row;
+//import org.apache.poi.ss.usermodel.Sheet;
+//import org.apache.poi.ss.usermodel.Workbook;
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+// 
+//public class ReadxlData {
+//	public Object[][] readXlData(String mvn) throws IOException{
+//		File file = new File(mvn);
+////		"C:\\Users\\2440634\\OneDrive - Cognizant\\Desktop\\DataProvider.xlsx"
+//		FileInputStream inputStream = new FileInputStream(file);
+//		Workbook book = new XSSFWorkbook(inputStream);
+//		Sheet sheet = book.getSheetAt(0);
+//		Object[][] data = new Object[sheet.getPhysicalNumberOfRows()-1][sheet.getRow(0).getLastCellNum()];
+//
+//		for(int i = 1; i < sheet.getLastRowNum(); i++) {
+//			Row row = sheet.getRow(i);
+//			for(int j = 0; j < row.getLastCellNum(); j++) {
+//				data[i - 1][j] = row.getCell(j).toString();
+//				System.out.println(row.getCell(j));
+//			}
+//		}
+//		return data;
+//	}
+//}
